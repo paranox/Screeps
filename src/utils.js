2984bc0f79da19a4209fef6771ff077ae914d75b
paranox <paranoxius@gmail.com>
2018-03-20 08:15:24 +0200

    Utils.objectToString() is now recursive on objects and arrays, with a maximum recursion depth limit.

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,35 @@
 ﻿var utils =
 {
-    objectToString: function(obj)
+    objectToString: function(obj, depth, maxDepth)
     {
-        return "{" + (Object.keys(obj).map(key => " " + key + ": " + obj[key])) + " }";
+        if (depth == undefined) depth = 0;
+        if (maxDepth == undefined) maxDepth = 3;
+
+        return "<" + depth + "/" + maxDepth + ">{" + (Object.keys(obj).map(key => {
+            if (Array.isArray(obj[key]) && depth < maxDepth)
+                return " " + key + ": " + this.arrayToString(obj[key], depth + 1, maxDepth);
+            if (typeof obj[key] == "object" && depth < maxDepth)
+                return " " + key + ": " + this.objectToString(obj[key], depth + 1, maxDepth);
+            if (typeof obj[key] == "function")
+                return " " + key + ": function()";
+            return " " + key + ": " + obj[key];
+        })) + " }";
+    },
+
+    arrayToString: function(array, depth, maxDepth)
+    {
+        if (depth == undefined) depth = 0;
+        if (maxDepth == undefined) maxDepth = 3;
+
+        return "<" + depth + "/" + maxDepth + ">[" + array.map(index => {
+            if (Array.isArray(obj[key]) && depth < maxDepth)
+                return this.arrayToString(array[index], depth + 1, maxDepth);
+            if (typeof obj[key] == "object" && depth < maxDepth)
+                return this.objectToString(array[index], depth + 1, maxDepth);
+            if (typeof obj[key] == "function")
+                return " " + key + ": function()";
+            return " " + array[index];
+        }) + " ]";
     },
 
     getBodyCost: function(bodyParts)
